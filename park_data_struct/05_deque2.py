371a6389fc4b7901bb3936afc99c9804dbc8e618
unknown <aaalwaysss0504@gmail.com>
2023-09-18 21:43:31 +0900

    week02.4

---
+++ park_data_struct/05_deque2.py
@@ -0,0 +1,30 @@
+class Deque:
+    def __init__(self):
+        self.container = list()
+
+    def is_empty(self):
+        if not self.container:
+            return True
+        else: 
+            return False
+    
+    def is_full():
+        pass
+
+    def insertFront():
+        pass
+
+    def insertRear():
+        pass
+
+    def popFront():
+        pass
+
+    def popRear():
+        pass
+
+    def peekFront():
+        pass
+
+    def peekRear():
+        pass
